0d27572e0b60fa6eb77ba96a78e18a8a28f5a71f
theirish81 <theirish81@gmail.com>
2016-10-25 16:36:41 +0200

    sonar script update

--- sonar.sh
+++ sonar.sh
@@ -2,4 +2,4 @@
 # (c) 20013-2016 API Fortress, Inc. The software in this package is published under the terms of the Commercial Free Software license V.1, a copy of which has been included with this distribution in the LICENSE.md file.
 #
 
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true && mvn sonar:sonar -Dsonar.scm.disabled=true
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dtest="*Test,*TestSuite" -Dmaven.test.failure.ignore=true && mvn sonar:sonar -Dsonar.scm.disabled=true
